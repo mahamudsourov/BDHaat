9b3c743396ff6816e211c9509105a990c25e22d0
mahamudsourov <sourovm554@gmail.com>
2025-06-17 01:30:56 +0600

    addedNavbar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BDHaaT</title>
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+</head>
+<body>
+
+    <!-- Navbar -->
+    <header class="navbar">
+    <div class="navbar-brand">
+        <img src="{{ asset('images/logo.png') }}" alt="BDHaaT Logo" /> 
+        <span>BDHaaT</span>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">District Products</a></li>
+            <li><a href="#">Healthy Foods</a></li>
+            <li><a href="#">Cart</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+    </nav>
+</header>
+
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,8 +14,8 @@
       <nav>
         <img src="{{ asset('images/logo.png') }}" alt="Logo" />
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
+          <li><a href="">Home</a></li>
+          <li><a href="{{ url('/about') }}">About</a></li>
           <li><a href="#">Products</a></li>
           <li><a href="#">Cities</a></li>
           <li><a href="#">Contacts</a></li>
