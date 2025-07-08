a82f86417786e3ef50cc8e6aa7d1ae04ccff2bf6
Nahid1828 <isam15-5334@diu.edu.bd>
2025-07-08 17:36:32 +0600

    conatct_page_with_nav

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,56 @@
+Contact Page:
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/contact.css') }}">
+    <title>BDHaat</title>
+</head>
+
+<body>
+
+    <section id="header">
+        <!-- Left: Logo + Name -->
+        <div id="logo-area">
+            <a href="{{ url('/') }}">
+                <img src="{{ asset('images/logo.png') }}" height="40" width="40" alt="Logo" />
+            </a>
+            <span class="brand-name">BDHaat</span>
+        </div>
+
+
+        <!-- Center: Search Bar + Search Icon + Cart -->
+        <div id="search-cart">
+            <div class="search-box">
+                <input type="text" placeholder="Search products..." id="search-bar" />
+                <button id="search-button"><i class="fas fa-search"></i></button>
+            </div>
+            <a href="cart.html" id="cart-icon"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+
+        <!-- Right: Navigation -->
+        <ul id="navbar">
+            <li><a href="{{ route('home') }}">Home</a></li>
+
+            <li class="dropdown">
+                <a href="#">Traditional Products</a>
+                <ul class="dropdown-menu">
+                    <li><a href="{{ route('clothes') }}">Clothes</a></li>
+                    <li><a href="{{ route('food') }}">Foods</a></li>
+                </ul>
+            </li>
+
+            <li><a href="{{ route('healthy_food') }}">Healthy Foods</a></li>
+            <li><a href="{{ route('contact') }}">Contact</a></li>
+        </ul>
+    </section>
+
+    
+</body>
+</html>
