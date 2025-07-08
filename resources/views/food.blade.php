3191103111c817839a34095224c2ac930fa2a21f
kouser-ahamed <kouserahamed.cse.diu@gmail.com>
2025-07-08 20:09:27 +0600

    Added Food Page

---
+++ resources/views/food.blade.php
@@ -0,0 +1,19 @@
+Food Page:
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <title>BDHaat</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
