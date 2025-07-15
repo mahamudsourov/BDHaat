8d46de09e3fbb15a9bb0f5cdc486b7ca9662ea4a
Nahid1828 <isam15-5334@diu.edu.bd>
2025-07-15 19:21:23 +0600

    product_details_footer

--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -92,8 +92,74 @@
         </div>
     </section>
 
+    <!-- Footer -->
+
+    <footer class="section-p1">
+        <div class="col">
+            <img class="logo" src="img/logo.png" alt="" />
+            <h4>Contact</h4>
+            <p>
+                <strong>Address: </strong>House 273, Road 13, Block C, Mirpur 6, Dhaka
+                1216
+            </p>
+            <p><strong>Phone</strong> +8801787087017 / +8801737611605</p>
+            <p><strong>Hours</strong> 8:00 AM - 11:59 PM, Sat - Thu</p>
+            <div class="follow">
+                <h4>Follow us</h4>
+                <div class="icon">
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-pinterest-p"></i>
+                    <i class="fab fa-youtube"></i>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <h4>About</h4>
+            <a href="#">About Us</a>
+            <a href="#">Delivery Information</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms & Condition</a>
+            <a href="#">Contact Us</a>
+        </div>
+
+        <div class="col">
+            <h4>My Account</h4>
+            <a href="#">Sign In</a>
+            <a href="#">View Cart</a>
+            <a href="#">My Wishlist</a>
+            <a href="#">Track My Order</a>
+            <a href="#">Help</a>
+        </div>
+
+        <div class="col install">
+            <h4>Install App</h4>
+            <p>From App Store or Google Play</p>
+            <div class="row">
+                <img src="img/pay/app.jpg" alt="" />
+                <img src="img/pay/play.jpg" alt="" />
+            </div>
+            <div class="secured-payment">
+                <p class="payment-title">Secured Payment Gateways</p>
+                <div class="payment-icons">
+                    <img src="{{ asset('images/bkash.png') }}" alt="Bkash">
+                    <img src="{{ asset('images/nagad.png') }}" alt="Nagad">
+                    <img src="{{ asset('images/visa.png') }}" alt="Visa">
+                    <img src="{{ asset('images/american-express.png') }}" alt="American Express">
+                </div>
+            </div>
+        </div>
+
+
+        <div class="copyright">
+            <p>&copy; BDHaat. All rights reserved.</p>
+        </div>
+    </footer>
 
     <script src="{{ asset('js/product-details.js') }}"></script>
 
 </body>
+
 </html>
