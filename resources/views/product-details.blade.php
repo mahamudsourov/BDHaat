f3f6ffca93b1f7573b58064f04e211cb28ed4997
Nahid1828 <isam15-5334@diu.edu.bd>
2025-07-15 19:18:41 +0600

    show_all_product_details

--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -71,8 +71,26 @@
             </ul>
     </section>
 
-    
+    <section id="prodetails" class="section-p1">
 
+        <div class="single-pro-image">
+            <img id="mainImg" src="" alt="Product Image">
+        </div>
+
+        <div class="single-pro-details">
+            <h6 id="brand"></h6>
+            <h4 id="title"></h4>
+            <h2 id="price"></h2>
+            <label for="quantity">Quantity:</label>
+            <input type="number" id="quantity" value="1" min="1" />
+            <br />
+            <button class="normal" onclick="addToCart()">Add to Cart</button>
+            <button class="normal" onclick="buyNow()">Buy Now</button>
+
+            <h4>Product Details</h4>
+            <p>High quality fabric, stylish design, made in Bangladesh.</p>
+        </div>
+    </section>
 
 
     <script src="{{ asset('js/product-details.js') }}"></script>
