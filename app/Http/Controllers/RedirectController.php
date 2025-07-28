7da79a440f21fef3fad36a5ec9cd7c1876c70a48
Nahid1828 <isam15-5334@diu.edu.bd>
2025-07-28 22:08:46 +0600

    Added_Redirect_Controller

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+
+class RedirectController extends Controller
+{
+    public function handle()
+    {
+        if (Auth::check()) {
+            if (Auth::user()->role === 'admin') {
+                return redirect()->route('admin.dashboard');
+            } else {
+                return redirect()->route('home');
+            }
+        }
+        return redirect('/login');
+    }
+}
