7251ff9eb5ac2ba51ec1b9d6907eafb73cb3a1de
Nahid1828 <isam15-5334@diu.edu.bd>
2025-07-28 21:28:51 +0600

    Log_in_for_Backend_html

---
+++ resources/views/login.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Login</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+
+</head>
+
+<body>
+    <div class="login-wrapper">
+        <h1>Login</h1>
+
+        @if ($errors->any())
+            <ul class="error-messages">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+
+        <form method="POST" action="{{ url('/login') }}">
+            @csrf
+            <input type="email" name="email" placeholder="Email" value="{{ old('email') }}" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+        </form>
+
+        <p>Don't have an account? <a href="{{ route('register') }}">Register here</a></p>
+    </div>
+</body>
+
+</html>
