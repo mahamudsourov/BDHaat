33291ecfa895c7be8d63c6f7beeef10eb7ee555a
Nahid1828 <isam15-5334@diu.edu.bd>
2025-07-28 21:58:20 +0600

    Model_User.php

---
+++ app/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+        'phone',
+        'address',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
