88f1fb88e5efbc9edf6e915f8d2caa896e0cf110
kouser-ahamed <kouserahamed.cse.diu@gmail.com>
2025-08-05 14:39:51 +0600

    Refactor help center: remove old help center view and update route to point to new help center page.

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::view('/about', 'about')->name('about');
 Route::view('/clothes', 'clothes')->name('clothes');
 Route::view('/food', 'food')->name('food');
 Route::view('/healthy_food', 'healthy_food')->name('healthy_food');
-Route::view('/helpcenter', 'user.helpcenter')->name('helpcenter');
+Route::view('/helpcenter', 'helpcenter')->name('helpcenter');
 Route::view('/customercare', 'customercare')->name('customercare');
 Route::view('/product-details', 'product-details')->name('product.details');
 Route::view('/cart', 'cart')->name('cart');
