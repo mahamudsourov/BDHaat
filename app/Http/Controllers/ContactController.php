5f09d2a73ece92d3484e66ab9664f7b0cf93001d
kouser-ahamed <kouserahamed.cse.diu@gmail.com>
2025-08-05 11:13:42 +0600

    Implement message storage and validation in ContactController

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -10,5 +10,21 @@ use Illuminate\Support\Facades\Validator;
 
 class ContactController extends Controller
 {
-    
+    public function store(Request $request){
+        $data = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email',
+            'message' => 'required|string|max:500',
+        ]);
+        if ($data->fails()) {
+            return response()->json(['errors' => $data->errors()], 422);
+        }
+        Message::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'subject' => $request->subject ?? 'No Subject',
+            'message' => $request->message,
+        ]);
+        return response()->json(['success' => 'Message sent successfully!'], 200);
+    }
 }
