c3b36d01a3d4daff7d872f9a814b6faf55388d04
kouser-ahamed <kouserahamed.cse.diu@gmail.com>
2025-08-05 11:11:46 +0600

    Add Message model and ContactController for message handling

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Message;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\View\View;
+use Illuminate\Support\Facades\Validator;
+
+class ContactController extends Controller
+{
+    
+}
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'subject',
+        'message'
+    ];
 }
