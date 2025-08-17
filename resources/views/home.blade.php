87cdf482d3dffca999a2c11c7bdf5a5a3322eb38
mr-taifur <taifurshishir123@gmail.com>
2025-08-17 21:20:08 +0600

    Updated_Dynamic_New Arrivals_In_Home_Fetching_from_Newly_Added_Products_in_Food,Clothes,Healthy_foods_Accordingly

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -162,130 +162,33 @@
 
         </div>
     </section>
-
     <!-- BD mart -->
-    <!-- New Arrival -->
+    
+            <!-- New Arrival -->
 
     <section id="product1" class="section-p1">
-        <h2>New Arrivals</h2>
-        <p>fresh fruits and Traditional Foods</p>
-        </p>
-        <div class="pro-container">
-            <!-- Cards with onclick to product details -->
-            <div class="pro" onclick="goToDetails('punjabi1.jpg', 'BDHaat-Authentic', 'Punjabi-1', 890)">
-                <img src="{{ asset('images/punjabi1.jpg') }}" alt="">
+    <h2>New Arrivals</h2>
+    <p>Authentic Clothes, Fresh Foods & Healthy Foods</p>
+    <div class="pro-container">
+        @foreach ($newArrivals as $item)
+            <div class="pro" onclick="goToDetails('{{ $item->image }}', '{{ $item->name }}', '{{ $item->description ?? '' }}', {{ $item->price }})">
+                <img src="{{ asset('storage/' . $item->image) }}" alt="{{ $item->name }}">
                 <div class="des">
-                    <span>BDHaat-Authentic</span>
-                    <h5>Punjabi-1</h5>
+                    <span>{{ class_basename($item) }}</span>
+                    <h5>{{ $item->name }}</h5>
                     <div class="star">
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>
                         <i class="fas fa-star"></i><i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i><i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
                     </div>
-                    <h4>BDT 890</h4>
+                    <h4>BDT {{ $item->price }}</h4>
                 </div>
                 <a href="#"><i class="fal fa-shopping-cart cart"></i></a>
             </div>
+        @endforeach
+    </div>
+</section>
 
-            <div class="pro" onclick="goToDetails('saree1.jpg', 'Tangail', 'Sari-1', 790)">
-                <img src="{{ asset('images/saree1.jpg') }}" alt="">
-                <div class="des">
-                    <span>Tangail</span>
-                    <h5>Sari-1</h5>
-                    <div class="star">
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i>
-                    </div>
-                    <h4>BDT 790</h4>
-                </div>
-                <a href="#"><i class="fal fa-shopping-cart cart"></i></a>
-            </div>
-
-            <div class="pro" onclick="goToDetails('sari2.webp', 'Tangail', 'Sari-2', 1050)">
-                <img src="{{ asset('images/sari2.webp') }}" alt="">
-                <div class="des">
-                    <span>Tangail</span>
-                    <h5>Sari-2</h5>
-                    <div class="star">
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i>
-                    </div>
-                    <h4>BDT 1050</h4>
-                </div>
-                <a href="#"><i class="fal fa-shopping-cart cart"></i></a>
-            </div>
-
-            <div class="pro" onclick="goToDetails('tripori_clothes.jpg', 'Bandarban', 'Tripori', 990)">
-                <img src="{{ asset('images/tripori_clothes.jpg') }}" alt="">
-                <div class="des">
-                    <span>Bandarban</span>
-                    <h5>Tripori</h5>
-                    <div class="star">
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i>
-                    </div>
-                    <h4>BDT 990</h4>
-                </div>
-                <a href="#"><i class="fal fa-shopping-cart cart"></i></a>
-            </div>
-
-            <div class="pro" onclick="goToDetails('sari5.jpeg', 'Dhaka', 'Sari-3', 840)">
-                <img src="{{ asset('images/sari5.jpeg') }}" alt="">
-                <div class="des">
-                    <span>Dhaka</span>
-                    <h5>Sari-3</h5>
-                    <div class="star">
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i>
-                    </div>
-                    <h4>BDT 840</h4>
-                </div>
-                <a href="#"><i class="fal fa-shopping-cart cart"></i></a>
-            </div>
-
-            <div class="pro" onclick="goToDetails('couple.jpg', 'Rangamati', 'Couple', 3080)">
-                <img src="{{ asset('images/couple.jpg') }}" alt="">
-                <div class="des">
-                    <span>Rangamati</span>
-                    <h5>Couple</h5>
-                    <div class="star">
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i>
-                    </div>
-                    <h4>BDT 3080</h4>
-                </div>
-                <a href="#"><i class="fal fa-shopping-cart cart"></i></a>
-            </div>
-
-            <div class="pro" onclick="goToDetails('punjabi2.webp', 'Dhaka', 'Panjabi-2', 1050)">
-                <img src="{{ asset('images/punjabi2.webp') }}" alt="">
-                <div class="des">
-                    <span>Dhaka</span>
-                    <h5>Panjabi-2</h5>
-                    <div class="star">
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i>
-                    </div>
-                    <h4>BDT 1050</h4>
-                </div>
-                <a href="#"><i class="fal fa-shopping-cart cart"></i></a>
-            </div>
-
-            <div class="pro" onclick="goToDetails('Tribal.jpg', 'Rangamati', 'Tribal-1', 1500)">
-                <img src="{{ asset('images/Tribal.jpg') }}" alt="">
-                <div class="des">
-                    <span>Rangamati</span>
-                    <h5>Tribal-1</h5>
-                    <div class="star">
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i><i class="fas fa-star"></i>
-                    </div>
-                    <h4>BDT 1500</h4>
-                </div>
-                <a href="#"><i class="fal fa-shopping-cart cart"></i></a>
-            </div>
-
-        </div>
-    </section>
 
 @endsection
 
